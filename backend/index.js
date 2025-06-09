607a51a1233084571750f47afcb420e75a80420d
Siddhesh-wagh <siddhesh.01092004@gmail.com>
2025-06-09 19:03:54 +0530

    Setup backend and frontend boilerplate

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Feedback Collection API is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
