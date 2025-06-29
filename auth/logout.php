51e66e1bed09aac747c35f42f1aed1b36e0e583e
Siddhesh-wagh <siddhesh.01092004@gmail.com>
2025-06-29 11:22:28 +0530

    log used while logout

--- auth/logout.php
+++ auth/logout.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+require_once __DIR__ . '/../db-config.php';
+require_once __DIR__ . '/../includes/functions.php';
+
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    logAction($conn, $user_id, "Logged out");
+}
+
 session_unset();
 session_destroy();
 
